b035901870a88df4c926d71c080e3758105c444c
dpkhang <khangphuc1@gmail.com>
2022-04-27 08:49:36 +0700

    modify product detaail

--- Blockchain-module/client/src/components/partials/Main/Content/TransactionDetails/description/Description.tsx
+++ Blockchain-module/client/src/components/partials/Main/Content/TransactionDetails/description/Description.tsx
@@ -1,52 +1,103 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './Description.scss'
+import $ from 'jquery'
 
 function Description() {
+
+
+    const handleClickToShow = (e: React.MouseEvent<HTMLElement>)=> {
+        console.log(e.currentTarget)
+      //  e.currentTarget.classList.add('active')
+        if(e.currentTarget) {
+            let target = $(e.currentTarget)
+            if(!target.hasClass('active')) {
+                $(e.currentTarget.lastChild as HTMLElement).css('transform', 'rotateZ(-270deg)')
+                $(e.currentTarget.nextElementSibling as HTMLElement).slideDown('.5s')
+                target.addClass('active')
+            } 
+            else{
+                $(e.currentTarget.lastChild as HTMLElement).css('transform', 'rotateZ(-90deg)')
+                $(e.currentTarget.nextElementSibling as HTMLElement).slideUp('.5s')
+                target.removeClass('active')
+            }
+        }
+    }
+
+
+    useEffect(()=> {
+        $('.transaction-description .detail-content').slideUp('.5s');
+        $('.transaction-description .generate-detail-item .fa-chevron-left').css('transform', 'rotateZ(-90deg)')
+    } ,[])
+
     return (
         <div className="transaction-description">
-            <p className="title">Mô tả</p>
+            <p className="title">Thông tin chi tiết</p>
             <div className="generate">
-                <p className="seeds">
-                    Giống lúa: <span>Đài thơm 8</span>
-                </p>
-                <p className="quantity">
-                    Số lượng: <span>200 tấn</span>
-                </p>
-                <p className="harvest-date">
-                    Ngày thu hoạch: <span>9-4-2022</span>
-                </p>
-                <p className="price">
-                    Giá: <span>10000000 VND</span>
-                </p>
-                <p className="area">
-                    Diện tích: <span>100 ha</span>
-                </p>
-                <p className="season">
-                    Mùa vụ: <span>Đông-xuân</span>
-                    <p className="season-detail">Xem chi tiết mùa vụ</p>
-                </p>
-            </div>
-            <div className="cooperation-info">
-                <p className="cooperation-title">Hợp tác xã</p>
-                <p className="cooperation-name">BÌNH MINH</p>
-                <p className="cooperation-description"><span>BINH MINH</span> là tổ chức kinh tế tập thể, đồng sở hữu, có tư cách pháp nhân, 
-                   và hợp tác tương trợ lẫn nhau trong hoạt động sản xuất. Hợp tác xã toạ lạc tại: <span>Ấp A - xã B -huyện C - tỉnh D, được quản lý bởi Nguyễn Văn A</span>, 
-                   gồm <span>N thành viên</span>. Để biết thêm chi tiết 
-                   vui lòng liên hệ qua số điện thoại: <span>091x.xxx.xxx</span> hoặc qua <span>HTX@gmail.com.</span></p>
-            </div>
-            <div className="buyer-info">
-                <p className="buyer-title">Thương lái</p>
-                <p className="buyer-name">LÚA MỚI</p>
-                <p className="buyer-description"><span>LUA MOI</span> là tổ chức thu mua lúa, toạ lạc tại: <span>Ấp A - xã B -huyện C - tỉnh D, được quản lý bởi Nguyễn Văn A</span>, 
-                   gồm <span>N thành viên</span>. Để biết thêm chi tiết 
-                   vui lòng liên hệ qua số điện thoại: <span>091x.xxx.xxx</span> hoặc qua <span>LT@gmail.com.</span></p>
-            </div>
-            <div className="sender-info">
-                <p className="sender-title">Người bán</p>
-                <p className="sender-name">NGUYỄN VĂN AN</p>
-                <p className="sender-description"><span>Ông: Nguyễn Văn A</span> là hộ sản xuất lúa, toạ lạc tại: <span>Ấp A - xã B -huyện C - tỉnh D</span> 
-                   . Để biết thêm chi tiết 
-                   vui lòng liên hệ qua số điện thoại: <span>091x.xxx.xxx</span> hoặc qua <span>LT@gmail.com.</span></p>
+                <div className="generate-item-wrapper">
+                    <div className="generate-detail-item" onClick={handleClickToShow}>
+                        <div className="generate-col">
+                            <i className="fa-solid fa-address-book"></i>
+                            <span>Chi tiết giao dịch</span>
+                        </div>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </div>
+                    <div className="detail-content detail-content-flex">
+                        <p>Giống lúa: <span>Nàng thơm</span></p>
+                        <p>Số lượng: <span>20 tấn</span></p>
+                        <p>Giá: <span>20000/kg</span></p>
+                        <p>Ngày thu hoạch: <span>12-4-2022</span></p>
+                        <p>Diện tích đất: <span>20 ha</span></p>
+                        <p>Mùa vụ: <span>Đông xuân</span></p>
+                    </div>
+                </div>
+                <div className="generate-item-wrapper">
+                    <div className="generate-detail-item" onClick={handleClickToShow}>
+                        <div className="generate-col">
+                            <i className="fa-solid fa-users"></i>
+                            <span>Chi tiết hợp tác xã</span>
+                        </div>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </div>
+                    <div className="detail-content">
+                        <p>Hợp tác xã: <span>BÌNH MINH</span></p>
+                        <p>Địa chỉ: <span>Mạc Thiên Tích, phường Xuân Khánh, Ninh Kiều Cần Thơ</span></p>
+                        <p>Trưởng hợp tác xã: <span>Nguyen Van A a a a a a a a a </span></p>
+                        <p>Số lượng thành viên: <span>20 thành viên</span></p>
+                        <p>Số điện thoại: <span>09393930549</span></p>
+                        <p>Email: <span>HTX@gmail.com</span></p>
+                    </div>
+                </div>
+                <div className="generate-item-wrapper">
+                    <div className="generate-detail-item" onClick={handleClickToShow}>
+                        <div className="generate-col">
+                            <i className="fa-solid fa-circle-user"></i>
+                            <span>Chi tiết thương lái</span>
+                        </div>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </div>
+                    <div className="detail-content">
+                        <p>Tên thương lái: <span>BÌNH MINH</span></p>
+                        <p>Địa chỉ: <span>Mạc Thiên Tích, phường Xuân Khánh, Ninh Kiều Cần Thơ</span></p>
+                        <p>Đại diện mua: <span>Nguyen Van A a a a a a a a a </span></p>
+                        <p>Số điện thoại: <span>09393930549</span></p>
+                        <p>Email: <span>TL@gmail.com</span></p>
+                    </div>
+                </div>
+                <div className="generate-item-wrapper">
+                    <div className="generate-detail-item" onClick={handleClickToShow}>
+                        <div className="generate-col">
+                            <i className="fa-solid fa-house-user"></i>
+                            <span>Chi thiết hộ sản xuất</span>
+                        </div>
+                        <i className="fa-solid fa-chevron-left"></i>
+                    </div>
+                    <div className="detail-content">
+                        <p>Hộ sản xuất: <span>Ông Nguyen Văn C</span></p>
+                        <p>Địa chỉ: <span>Mạc Thiên Tích, phường Xuân Khánh, Ninh Kiều Cần Thơ</span></p>
+                        <p>Số điện thoại: <span>09393930549</span></p>
+                        <p>Email: <span>TL@gmail.com</span></p>
+                    </div>
+                </div>
             </div>
         </div>
     )
