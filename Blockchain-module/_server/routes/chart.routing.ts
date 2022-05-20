764782285ccb2a1f4a69998333c5dd1ae5654f33
dpkhang <khangphuc1@gmail.com>
2022-05-20 20:21:23 +0700

    create init front end app

--- Blockchain-module/_server/routes/chart.routing.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express'
-
-import { getDataChart } from '../controllers/chart.controller'
-
-const route = express.Router()
-
-route.get('/', getDataChart)
-
-export default route
