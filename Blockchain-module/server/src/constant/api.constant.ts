75e52c800ed09b3e9ff3d60399f89b69aadcc6f1
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-05-24 00:43:53 +0700

    update model object

--- Blockchain-module/server/src/constant/api.constant.ts
+++ Blockchain-module/server/src/constant/api.constant.ts
@@ -5,6 +5,10 @@ const ControllerConstant = {
   thuocbaovethucvat: '/api/thuocbaovethucvat',
   giaodich: '/api/giaodich',
   lohang: '/api/lohang',
+  hoptacxa: '/api/hoptacxa',
+  gionglua: '/api/gionglua',
+  lichmuavu: '/api/lichmuavu',
+  nhatkydongruong: '/api/nhatkydongruong'
 };
 
 const MethodConstant = {
