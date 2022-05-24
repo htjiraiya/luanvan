bbe7b09f67e8fb9f7af48cb8c6e11da10a341299
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-05-25 00:14:25 +0700

    initial router

--- Blockchain-module/client/admin/src/App.tsx
+++ Blockchain-module/client/admin/src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate
+} from "react-router-dom"
+import Login from './components/layouts/login/login'
+import Main from './components/layouts/main/main'
+import NotFound from './components/layouts/not-found/not-found'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/login' element={<Login/>}></Route>
+          <Route path='main/*' element={<Main/>}></Route>
+          <Route path='' element={<Navigate to='/main'/>}></Route>
+          <Route path='*' element={<NotFound/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ Blockchain-module/client/admin/src/components/layouts/main/main.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import {
+    Routes,
+    Route,
+    Navigate
+} from 'react-router-dom'
+import Menu from '../../partials/menu/menu';
+import Footer from '../../partials/footer/footer';
+import Dashboard from '../../partials/dashboard/dashboard';
+import Products from '../../partials/products/products';
+import AddProduct from '../../partials/add-product/add-product';
+import ProductDetails from '../../partials/product-details/product-details';
+import TransactionDetails from '../../partials/transaction-details/transaction-details';
+import Transactions from '../../partials/transactions/transactions';
+import AddTransaction from '../../partials/add-transaction/add-transaction';
+import Profile from '../../partials/profile/profile';
+import NotFound from '../not-found/not-found';
+
+const Main = () => {
+    return (
+        <>
+            <Menu />
+            <Routes>
+                <Route path='/dashboard' element={<Dashboard/>}></Route>
+                <Route path='/products' element={<Products/>}></Route>
+                <Route path='/add-product' element={<AddProduct/>}></Route>
+                <Route path='/product-details' element={<ProductDetails/>}></Route>
+                <Route path='/transactions' element={<Transactions/>}></Route>
+                <Route path='/add-transaction' element={<AddTransaction/>}></Route>
+                <Route path='/transaction-details' element={<TransactionDetails/>}></Route>
+                <Route path='/profile' element={<Profile/>}></Route>
+                <Route path='/' element={<Navigate to='/main/dashboard'/>}></Route>
+                <Route path='/*' element={<NotFound/>}></Route>
+            </Routes>
+            <Footer />
+        </>
+    );
+};
+
+export default Main;
