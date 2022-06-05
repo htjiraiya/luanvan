d383c11dc92e03768c4317ea323d7fcd306a1721
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-06-05 20:50:32 +0700

    added menu and footer

--- Blockchain-module/client/admin/src/components/layouts/main/main.tsx
+++ Blockchain-module/client/admin/src/components/layouts/main/main.tsx
@@ -15,24 +15,33 @@ import Transactions from '../../partials/transactions/transactions';
 import AddTransaction from '../../partials/add-transaction/add-transaction';
 import Profile from '../../partials/profile/profile';
 import NotFound from '../not-found/not-found';
+import Background from '../../partials/background/background';
+import Header from '../../partials/header/header';
+import './main.scss';
 
 const Main = () => {
     return (
         <>
             <Menu />
-            <Routes>
-                <Route path='/dashboard' element={<Dashboard/>}></Route>
-                <Route path='/products' element={<Products/>}></Route>
-                <Route path='/add-product' element={<AddProduct/>}></Route>
-                <Route path='/product-details' element={<ProductDetails/>}></Route>
-                <Route path='/transactions' element={<Transactions/>}></Route>
-                <Route path='/add-transaction' element={<AddTransaction/>}></Route>
-                <Route path='/transaction-details' element={<TransactionDetails/>}></Route>
-                <Route path='/profile' element={<Profile/>}></Route>
-                <Route path='/' element={<Navigate to='/main/dashboard'/>}></Route>
-                <Route path='/*' element={<NotFound/>}></Route>
-            </Routes>
-            <Footer />
+            <div className="main-wrapper">
+                <Header />
+                <div className="main-content">
+                    <Routes>
+                        <Route path='/dashboard' element={<Dashboard />}></Route>
+                        <Route path='/products' element={<Products />}></Route>
+                        <Route path='/add-product' element={<AddProduct />}></Route>
+                        <Route path='/product-details' element={<ProductDetails />}></Route>
+                        <Route path='/transactions' element={<Transactions />}></Route>
+                        <Route path='/add-transaction' element={<AddTransaction />}></Route>
+                        <Route path='/transaction-details' element={<TransactionDetails />}></Route>
+                        <Route path='/profile' element={<Profile />}></Route>
+                        <Route path='/' element={<Navigate to='/main/dashboard' />}></Route>
+                        <Route path='/*' element={<NotFound />}></Route>
+                    </Routes>
+                    <Footer />
+                </div>
+            </div>
+            <Background />
         </>
     );
 };
