cdaf07e371d0cd9b06d4a24db7f318d5ea63219b
maitrongnhan001 <maitrongnhan.001@gmail.com>
2022-06-06 23:37:17 +0700

    added layout product

--- Blockchain-module/client/admin/src/components/partials/add-product/add-product.tsx
+++ Blockchain-module/client/admin/src/components/partials/add-product/add-product.tsx
@@ -1,9 +1,34 @@
+import { MDBFile, MDBInput } from 'mdb-react-ui-kit';
 import React from 'react';
+import './add-product.scss';
 
 const AddProduct = () => {
     return (
-        <div>
-            add product
+        <div className='add-product-wrapper'>
+            <br />
+            <h1>Thêm lô hàng</h1>
+            <form action="" className='form-add-product'>
+                <div className="input-product">
+                    <MDBInput label='Tên lô hàng' id='form1' type='text' />
+                </div>
+                <br />
+                <div className="input-product">
+                    <MDBInput label='Tên lô hàng' id='form1' type='text' />
+                </div>
+                <br />
+                <div className="quantity-price">
+                    <div className="input-product">
+                        <MDBInput label='Tên lô hàng' id='form1' type='text' />
+                    </div>
+                    <div className="input-product">
+                        <MDBInput label='Tên lô hàng' id='form1' type='text' />
+                    </div>
+                </div>
+                <br />
+                <div className='file-container'>
+                    <MDBFile label='Default file input example' id='customFile' />
+                </div>
+            </form>
         </div>
     );
 };
--- Blockchain-module/client/admin/src/index.tsx
+++ Blockchain-module/client/admin/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { store } from './redux/app/store';
 import { Provider } from 'react-redux';
+import 'mdb-react-ui-kit/dist/css/mdb.min.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
